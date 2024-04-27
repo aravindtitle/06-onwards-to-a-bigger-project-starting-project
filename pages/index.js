edb3968fbe1aa8ef8a201737e1c36a07c5998aad
aravindtitle <aravindtitle@gmail.com>
2024-04-27 19:36:14 +0530

    Prerendering the powers of Next JS

--- pages/index.js
+++ pages/index.js
@@ -4,20 +4,39 @@ const DUMMY_MEETUPS = [
   {
     id: "m1",
     title: "A First Meetup",
-    image: "url",
-    address: "address",
-    description: "this is a first meetup! ",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "Some address 5, 12345 Some City",
+    description: "This is a first meetup!",
   },
   {
     id: "m2",
     title: "A Second Meetup",
-    image: "url",
-    address: "address",
-    description: "this is a Second meetup! ",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "Some address 10, 12345 Some City",
+    description: "This is a second meetup!",
+  },
+  {
+    id: "m3",
+    title: "A Third Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "Some address 15, 12345 Some City",
+    description: "This is a third meetup!",
   },
 ];
-function HomePage() {
-  return <MeetupList meetups={DUMMY_MEETUPS} />;
+
+function HomePage(props) {
+  return <MeetupList meetups={props.meetups} />;
+}
+
+export async function getStaticProps(context) {
+  return {
+    props: {
+      meetups: DUMMY_MEETUPS,
+    },
+  };
 }
 
 export default HomePage;
